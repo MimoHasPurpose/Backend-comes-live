63fe7ead5ee15970a38f8a586763e5f8007ee25b
Sanjay Singh <77540699+Sanj-bot@users.noreply.github.com>
2025-02-24 15:34:29 +0530

    update

--- mongoose/models/hospital-management/doctor.models.js
+++ mongoose/models/hospital-management/doctor.models.js
@@ -1,5 +1,29 @@
 import mongoose from 'mongoose';
-const hospitalSchema=new mongoose.Schema({},{timestamps:true});
+const hospitalSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    salary:{
+        type:String,
+        required:true
+    },
+    qualification:{
+        type:String,
+        required:true
+    },
+    experienceInYears:{
+        type:Number,
+        default:0
+    },
+    worksInHospitals:[
+        {
+            type:mongoose.Schema.Types.OBjectId,
+            ref:"Hospital"
+        }
+    ]
+
+},{timestamps:true});
 
 export const Hospital=mongoose.model(
     'Hospital',
--- mongoose/models/hospital-management/patient.models.js
+++ mongoose/models/hospital-management/patient.models.js
@@ -7,6 +7,28 @@ const patientSchema=new mongoose.Schema({
     diagnosedWith:{
         type:String,
         required:true
+    },
+    address:{
+        type:String,
+        required:true
+    },
+    age:{
+        type:Number,
+        required:true
+    },
+    bloodGroup:{
+        type:String,
+        required:true
+    },
+    gender:{
+        type:String,
+        enum:["M","F","O"],
+        reuired:true
+    },
+    admittedIn:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Hospital",
     }
+
 },{timestamps:true});
 export const Patient=mongoose.model('Patient',patientSchema);
