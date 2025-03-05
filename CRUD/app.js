c338bc25b04973ae2f1db7b8aa580e464c3050b1
Sanjay Singh <77540699+Sanj-bot@users.noreply.github.com>
2025-03-05 15:41:24 +0530

    up

--- CRUD/app.js
+++ CRUD/app.js
@@ -1,5 +1,6 @@
 const express=require('express');
 const app=express();
+const path=require('path')
 
 app.set("view engine","ejs");
 app.use(express.json());
@@ -8,7 +9,7 @@ app.use(express.static(path.join(__dirname,'public')))
 
 
 app.get('/',(req,res)=>{
-    res.send('hey!')
+    res.render('index')
 })
 
 app.listen(3000);
