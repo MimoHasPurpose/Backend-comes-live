3021dd805af42a25ab395b65a8d21cb9e911262b
Sanjay Singh <77540699+MimoHasPurpose@users.noreply.github.com>
2025-03-07 09:52:13 +0530

    update

--- CRUD/app.js
+++ CRUD/app.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const app=express();
 const path=require('path');
+const userModel=require('./models/user')
 
 app.set("view engine","ejs");
 app.use(express.json());
@@ -10,10 +11,19 @@ app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/',(req,res)=>{
     res.render('index');
-})
+});
 app.get('/read',(req,res)=>{
     res.render("read");
-})
+});
+app.post('/create',async(req,res)=>{
+    let{name,username,image}=req.body;
+    let createdUser= await userModel.create({
+        name,
+        email,
+        image
+    })
+    res.send(createdUser);
+});
 app.listen(3000);
 
 
