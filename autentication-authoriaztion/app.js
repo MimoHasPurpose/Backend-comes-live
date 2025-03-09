c1d77be70d3a65de7d561c7ba8bcd6ecfb9f236a
Sanjay Singh <77540699+MimoHasPurpose@users.noreply.github.com>
2025-03-09 11:44:02 +0530

    jwt+bcrypt+cookie-parser done

--- autentication-authoriaztion/app.js
+++ autentication-authoriaztion/app.js
@@ -2,12 +2,30 @@ const cookieParser = require('cookie-parser');
 const express=require('express');
 const app=express();
 const bcrypt=require('bcrypt');
+const jwt=require('jsonwebtoken');
 let a="";
 app.use(cookieParser());
 app.get('/',function(req,res){
-    res.cookie("name","harsh");
+    // res.cookie("name","harsh");
     res.send("done");
 })
+
+app.get("/jwt",function(req,res){
+    let token=jwt.sign({email:"sanjay@gmail.com"},"secretshouldnotbeinplainletters")
+    console.log(token);
+    res.cookie("token",token);
+    res.send("done");
+    
+});
+
+app.get("/readjwt",function(req,res){
+     console.log(req.cookies.token);
+     
+     let data=jwt.verify(req.cookies.token,"secretshouldnotbeinplainletters");
+     res.send("<i>req.cookies.token </i> "+req.cookies.token+" <br><i>data is: </i>"+data.email);
+    console.log(data);
+})
+
 app.get("/password",function(req,res){
     bcrypt.genSalt(10, function(err, salt) {
         bcrypt.hash("mimo", salt, function(err, hash) {
